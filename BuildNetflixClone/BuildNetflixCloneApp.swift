e7ff1075508d3b0cc7df3d467c5fe9080288e5b0
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-11-02 23:45:38 +1300

    Initial Commit

---
+++ BuildNetflixClone/BuildNetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixCloneApp.swift
+//  BuildNetflixClone
+//
+//  Created by Paulo Orquillo on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildNetflixCloneUITests/BuildNetflixCloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BuildNetflixCloneUITestsLaunchTests.swift
+//  BuildNetflixCloneUITests
+//
+//  Created by Paulo Orquillo on 2/11/21.
+//
+
+import XCTest
+
+class BuildNetflixCloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
